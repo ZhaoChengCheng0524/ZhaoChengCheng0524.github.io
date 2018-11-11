e17b77c624829bfd2e12c57e577b41116a85255c
赵晨晨 <1310393537@qq.com>
2018-11-11 13:44:27 +0800

    2018111101

--- web/js/script.js
+++ web/js/script.js
@@ -9,15 +9,13 @@ $(document).ready(function(){
     });
     var maxtime = 60 * 60; //一个小时，按秒计算，自己调整!
     if (maxtime >= 0) {
-                    minutes = Math.floor(maxtime / 60);
-                    seconds = Math.floor(maxtime % 60);
-                    msg = "距离结束还有" + minutes + "分" + seconds + "秒";
-                    document.all["timer"].innerHTML = msg;
-                    if (maxtime == 5 * 60)alert("还剩5分钟");
-                        --maxtime;
-                } else{
-                    clearInterval(timer);
-                    alert("时间到，结束!");
+        minutes = Math.floor(maxtime / 60);
+        seconds = Math.floor(maxtime % 60);
+        msg = "only " + minutes + "minutes " + seconds + "seconds";
+        document.all["timer"].innerHTML = msg;
+    }
+    else {
+        clearInterval(timer);
     }
     timer = setInterval("CountDown()", 1000);
     /*
