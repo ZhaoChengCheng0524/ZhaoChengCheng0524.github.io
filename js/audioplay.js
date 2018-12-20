25d060a44cf48390b325ddf225ff7f6f2e588929
赵晨晨 <1310393537@qq.com>
2018-12-20 22:14:20 +0800

    fix

--- js/audioplay.js
+++ js/audioplay.js
@@ -1,12 +1,13 @@
 var player=document.getElementById("audioPlayer");
-
+var info="卡农是两段重合的音乐，到了高潮后乐终于追上了前乐……爱情也是这样，为了对方让自己变成更好的一个人，希望最终能够天长地久。";
+var all_info="希望在未来的时光，能和你一起快乐的走下去。——致最可爱的小可爱";
 function musicTips(){
 	firm();
 }
-//弹出一个询问框，有确定和取消按钮  
 function firm() {   
-	if (confirm("是否播放背景音乐？")) {  
+	if (confirm("是否播放背景音乐？")) {
 		player.play();
+		console.info(info);
 	}  
-
+	console.info(all_info);
 } 
